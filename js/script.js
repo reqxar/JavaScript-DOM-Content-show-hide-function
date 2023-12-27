16cb6c7bd1ce2e3f6dee6e6bb5b4d5d1bd57d09b
Юрий <84281981+reqxar@users.noreply.github.com>
2023-12-27 22:04:32 +0300

    Update script.js
    Вывод и скрытие модального окна при нажатии на кнопку

--- js/script.js
+++ js/script.js
@@ -42,7 +42,7 @@ window.addEventListener("DOMContentLoaded", function () {
   });
 
   //Назначение даты окончания.
-  let deadline = "2023-12-11";
+  let deadline = "2023-12-31";
 
   //Функция получения оставшегося времени до даты окончания.
   function getRemainigTime(endtime) {
@@ -90,4 +90,25 @@ window.addEventListener("DOMContentLoaded", function () {
   }
   
   setClock("timer", deadline);
+
+
+  //Вывод и скрытие модального окна при нажатии на кнопку
+
+  let more = document.querySelector('.more'),
+      overlay = document.querySelector('.overlay'),
+      close = document.querySelector('.popup-close');
+
+
+  more.addEventListener('click', function() {
+    overlay.style.display = 'block';
+    this.classList.add('more-splash');
+    document.body.style.overflow = 'hidden';
+  })
+  
+  close.addEventListener('click', function(){
+    overlay.style.display = 'none';
+    more.classList.remove('more-splash');
+    document.body.style.overflow = '';
+  })
+  
 });
